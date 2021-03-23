29aab6fcf0ec56a81e9d1698c7bf58c7540b90c7
Dave Spina <dspina79@gmail.com>
2021-03-23 00:34:01 -0400

    Handled new pointer functionality with a struct

--- structpointers.c
+++ structpointers.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 struct ClassGrade {
     char grade;
@@ -8,7 +9,19 @@ struct ClassGrade {
     int classid;
 };
 
+struct Novel {
+    char author[50];
+    char title[100];
+    int yearPublished;
+    int numTimesPrinted;
+};
+
+
+
 void printDetails(struct ClassGrade grade);
+void printBook(struct Novel *novel);
+struct Novel getNewNovel();
+
 int main() {
     struct ClassGrade grade;
     printf("Supply the grade: ");
@@ -17,9 +30,29 @@ int main() {
     grade.studentid = 8739245;
     grade.teacherId = 6890;
     printDetails(grade);
+
+    struct Novel n = getNewNovel();
+    printBook(&n);
+    printBook(&n);
     return 0;
 }
 
 void printDetails(struct ClassGrade grade) {
     printf("Congratulations, the grade is a %c\n", grade.grade);
+}
+
+struct Novel getNewNovel() {
+    struct Novel n;
+    strcpy(n.author, "Stephen King");
+    strcpy(n.title,"The Stand");
+    n.yearPublished = 1980;
+    n.numTimesPrinted = 0;
+
+    return n; 
+}
+
+void printBook(struct Novel *novel) {
+    printf("Printing the novel %s\n", novel->title);
+    novel->numTimesPrinted++;
+    printf("The novel has now been printed here %d times\n", novel->numTimesPrinted);
 }
