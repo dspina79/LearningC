a7cdcb6d49a959efe5af6a0e12a54391dbfb22eb
Dave Spina <dspina79@gmail.com>
2021-04-01 21:05:27 -0400

    Initial code for stats

---
+++ stats.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+float avg(int *, int N);
+
+int main() {
+    int arr[1];
+    int x = 0, N = 0;
+
+    while (true) {
+        printf("Enter a number. Use -1 to exit. >> ");
+        scanf("%d", x);
+        if (x == -1) {
+            break;
+        }
+
+
+    }
+
+    if (N == 0) {
+        printf("Nothing to see here. Nothing was entered.\n");
+        return 0; 
+    }
+
+    // determine the mean
+
+    return 0;
+}
+
+
